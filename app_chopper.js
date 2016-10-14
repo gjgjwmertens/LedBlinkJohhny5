eb576681dc44ecdf5a8e79813bcc41af8a838b86
gjgjwmertens <G.J.G.J.W.Mertens@uva.nl>
2016-10-14 13:37:12 +0200

    added motor control

--- app_chopper.js
+++ app_chopper.js
@@ -4,10 +4,16 @@ var board = new five.Board();
 board.on("ready", function() {
 
    var sensor = new five.Sensor('A0');
+   var motor = new five.Motor(9);
 
    sensor.on('change', function() {
       process.stdout.write('\033c');
       console.log('[' + Date.now() + ']' + this.scaleTo(0, 10));
       console.log('[' + Date.now() + ']' + this.value);
+   });
+
+   motor.start(150);
+   board.wait(5000, function() {
+      motor.stop();
    });
 });
